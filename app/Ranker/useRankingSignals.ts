984df0e64f392e207d8eddf1e8086ffcbcc7cbbd
Jake Archibald <jaffathecake@gmail.com>
2025-09-25 17:38:08 +0100

    Shuffle unranked items

--- app/Ranker/useRankingSignals.ts
+++ app/Ranker/useRankingSignals.ts
@@ -8,6 +8,24 @@ import allItems from '../data.json';
 export const itemsById = new Map<number, RankingItem>();
 for (const item of allItems) itemsById.set(item.id, item);
 
+function shuffle(array: unknown[], rand: () => number = Math.random) {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(rand() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
+
 export function useRankingSignals(user: User): {
   rankedItems: Signal<RankingItem[]>;
   unrankedItems: Signal<RankingItem[]>;
@@ -36,7 +54,10 @@ export function useRankingSignals(user: User): {
 
   const initialUnrankedItems = useMemo<RankingItem[]>(() => {
     const rankingIdsSet = new Set(user.rankings);
-    return allItems.filter((item) => !rankingIdsSet.has(item.id));
+    const unranked = allItems.filter((item) => !rankingIdsSet.has(item.id));
+    shuffle(unranked);
+
+    return unranked;
   }, [user.rankings]);
 
   const rankedItems = useSignal<RankingItem[]>(initialRankedItems);
